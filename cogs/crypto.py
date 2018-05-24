0229c9af50b485238f5df807f816f8450e712560
Jeff Cailteux <jethro82@gmail.com>
2018-05-23 21:28:06 -0700

    ven and .idea bs

--- cogs/crypto.py
+++ cogs/crypto.py
@@ -7,11 +7,13 @@ from collections import Counter, defaultdict, namedtuple
 import discord
 import time
 import os
-import urllib.request 
+import urllib.request
 import json
+
 
 class Crypto:
     """General commands."""
+
     def __init__(self, bot):
         self.bot = bot
         self.data = ""
@@ -19,37 +21,36 @@ class Crypto:
         self.last_time = 0
 
     @commands.group(pass_context=True)
-    async def crypto(self, ctx, *, term: str=""):
+    async def crypto(self, ctx, *, term: str = ""):
         """Crypto"""
 
-        if (term.lower() == "jeth"):
+        if term.lower() == "jeth":
             await self.bot.say("```JETH is priceless```")
             return
 
-        if (term.lower() == "meth"):
+        if term.lower() == "meth":
             await self.bot.say("```Don't do drugs kids```")
             return
 
-        await getListings(self)
-        cryptoId = 0
+        await get_listings(self)
+        crypto_id = 0
 
         for crypto in self.listings['data']:
-            if term.lower() == crypto['name'].lower():    
-                cryptoId = crypto['id']
+            if term.lower() == crypto['name'].lower():
+                crypto_id = crypto['id']
                 break
-            if term.lower() == crypto['symbol'].lower():    
-                cryptoId = crypto['id']
+            if term.lower() == crypto['symbol'].lower():
+                crypto_id = crypto['id']
                 break
-            if term.lower() == crypto['website_slug'].lower():    
-                cryptoId = crypto['id']
+            if term.lower() == crypto['website_slug'].lower():
+                crypto_id = crypto['id']
                 break
 
-        if cryptoId == 0:
+        if crypto_id == 0:
             await self.bot.say("```" + term + " not found" + "```")
             return
 
-        msg = ""
-        crypto_url = "https://api.coinmarketcap.com/v2/ticker/" + str(cryptoId) + "/"
+        crypto_url = "https://api.coinmarketcap.com/v2/ticker/" + str(crypto_id) + "/"
         with urllib.request.urlopen(crypto_url) as req:
             crypto = json.loads(req.read().decode())['data']
             msg_price = "$" + str(crypto['quotes']['USD']['price']) + "/" + crypto['symbol']
@@ -57,12 +58,14 @@ class Crypto:
             msg_rank = "#" + str(crypto['rank'])
             await self.bot.say("```" + msg_price + " " + msg_percent + " " + msg_rank + "```")
             return
-        
+
+
 def setup(bot):
     bot.add_cog(Crypto(bot))
 
-async def getListings(self):
+
+async def get_listings(self):
     listings_url = "https://api.coinmarketcap.com/v2/listings"
     if self.listings == "":
         with urllib.request.urlopen(listings_url) as req:
-            self.listings = json.loads(req.read().decode())        
+            self.listings = json.loads(req.read().decode())
