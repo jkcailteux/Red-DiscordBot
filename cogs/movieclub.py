b0780561d979ee0cd03a58960ac54d1855ff4087
Jeff Cailteux <jethro82@gmail.com>
2018-05-25 21:14:35 -0700

    verify input, caching and files

--- cogs/movieclub.py
+++ cogs/movieclub.py
@@ -1,19 +1,34 @@
+import json
+import logging
+import os
+import urllib.request
+
 import discord
 from discord.ext import commands
-from .utils.dataIO import fileIO
+
 from .utils import checks
-import os
-import logging
-import urllib.request
-import json
+from .utils.dataIO import fileIO
+
+
+class Files:
+    data_movieclub = "data/movieclub"
+    rated_movie_list_json = data_movieclub + "/rated_movielist.json"
+    movie_list_json = data_movieclub + "/movielist.json"
+
+
+class Api:
+    api_key = "&apikey=cfe10214"
+    omdb_url = "https://www.omdbapi.com/?i="
 
 
 class MovieClub:
 
     def __init__(self, bot):
         self.bot = bot
-        self.movie_list = fileIO("data/movieclub/movielist.json", "load")
+        self.movie_list = list(fileIO(Files.movie_list_json, "load"))
+        self.rated_movie_list = set(fileIO(Files.rated_movie_list_json, "load")["_set_object"])
         self.omdb_key = "cfe10214"
+        self.movie_cache = {}
 
     @commands.command(pass_context=True)
     @checks.serverowner_or_permissions(administrator=True)
@@ -26,11 +41,11 @@ class MovieClub:
                 # We have a valid movie, save it
                 # Movie is inserted into first position
                 # Todo support movie history
-                self.movie_list.insert(0, movie)
+                self.movie_list.insert(0, movie_id)
 
                 await self.bot.say("Movie set to ....")
                 await create_movie_embed(self, movie)
-                fileIO("data/movieclub/movielist.json", "save", self.movie_list)
+                fileIO(Files.movie_list_json, "save", self.movie_list)
             else:
                 msg = "Movie " + movie_id + " not found"
         else:
@@ -42,9 +57,10 @@ class MovieClub:
     @commands.command(pass_context=True)
     async def movieget(self, ctx):
 
-        if self.movie_list.__sizeof__() > 0:
+        if self.movie_list.__len__() > 0:
             await self.bot.say("Current Movie is .....")
-            await create_movie_embed(self, self.movie_list[0])
+            movie = await get_movie(self, movie_id=self.movie_list[0])
+            await create_movie_embed(self, movie)
         else:
             await self.bot.say("There is no current movie")
 
@@ -54,6 +70,38 @@ class MovieClub:
 
         movie = await get_movie(self, movie_id)
 
+        # Validate Input
+        error_msg = ""
+
+        if movie is None:
+            error_msg = "Invalid movie id: " + movie_id
+        if plot < 0 or plot > 10:
+            error_msg = "Plot needs to be between 0 and 10"
+        if acting < 0 or acting > 10:
+            error_msg = "Acting needs to be between 0 and 10"
+        if visuals < 0 or visuals > 10:
+            error_msg = "Visuals needs to be between 0 and 10"
+        if sound < 0 or sound > 10:
+            error_msg = "Sound needs to be between 0 and 10"
+
+        rewatchable_score = parse_binary_text(rewatchable)
+        if rewatchable_score is None:
+            error_msg = "Rewatchable needs to yes/no, true/false, or 1/0"
+
+        entertaing_score = parse_binary_text(entertaing)
+        if entertaing_score is None:
+            error_msg = "Entertaining needs to yes/no, true/false, or 1/0"
+
+        if error_msg is not "":
+            await self.bot.say(
+                "Error submitting report by " + "<@" + ctx.message.author.id + ">")
+            await self.bot.say("```" + error_msg + "```")
+            return
+
+        # We have a valid movie rating
+        self.rated_movie_list.add(movie_id)
+        fileIO(Files.rated_movie_list_json, "save", dict(_set_object=list(self.rated_movie_list)))
+
         msg = "Movie Rating for `" + movie["Title"] + "` by `" + ctx.message.author.name + "`\n\n"
         msg += "```"
         msg += "Plot:        " + str(plot) + "\n"
@@ -61,11 +109,11 @@ class MovieClub:
         msg += "Visuals:     " + str(visuals) + "\n"
         msg += "Sound:       " + str(sound) + "\n"
         msg += "\n"
-        msg += "Rewatchable: " + str(rewatchable) + "\n"
-        msg += "Entertaing:  " + str(entertaing) + "\n"
+        msg += "Rewatchable: " + str(rewatchable_score) + "\n"
+        msg += "Entertaing:  " + str(entertaing_score) + "\n"
         msg += "```"
 
-        await self.bot.say(msg)
+        await self.bot.whisper(msg)
 
 
 # plot*2 + visual + sound + acting + 5*rewatchable + 5*entertaining
@@ -89,22 +137,39 @@ async def create_movie_embed(self, movie):
 
 
 async def get_movie(self, movie_id: str):
-    omdb_url = "https://www.omdbapi.com/?i=" + movie_id + "&apikey=" + self.omdb_key
+    if movie_id in self.movie_cache:
+        return self.movie_cache[movie_id]
+
+    omdb_url = Api.omdb_url + movie_id + Api.api_key
     with urllib.request.urlopen(omdb_url) as req:
-        return json.loads(req.read().decode())
+        movie = json.loads(req.read().decode())
+        self.movie_cache[movie_id] = movie
+        return movie
+
+
+def parse_binary_text(text: str):
+    if text.lower() == "yes" or text.lower() == "true" or text.lower() == "1":
+        return True
+    if text.lower() == "no" or text.lower() == "false" or text.lower() == "0":
+        return False
+    else:
+        return None
 
 
 def check_folders():
-    if not os.path.exists("data/movieclub"):
+    if not os.path.exists(Files.data_movieclub):
         print("Creating data/movieclub folder...")
-        os.makedirs("data/movieclub")
+        os.makedirs(Files.data_movieclub)
 
 
 def check_files():
-    f = "data/movieclub/movielist.json"
-    if not fileIO(f, "check"):
+    if not fileIO(Files.movie_list_json, "check"):
         print("Creating empty movielist.json...")
-        fileIO(f, "save", [])
+        fileIO(Files.movie_list_json, "save", [])
+
+    if not fileIO(Files.rated_movie_list_json, "check"):
+        print("Creating empty rated_movielist.json...")
+        fileIO(Files.rated_movie_list_json, "save", {})
 
 
 def setup(bot):
