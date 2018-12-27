e217a44c34e9a03a52e32f47522f229917f51179
Jeff Cailteux <jethro82@gmail.com>
2018-12-27 15:45:30 -0800

    stocks cog

--- cogs/stocks.py
+++ cogs/stocks.py
@@ -23,19 +23,33 @@ class Stocks:
     async def stocks(self, ctx, *, term: str = ""):
         """Stocks"""
 
-        stock_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + \
-            str(term)
+        stock_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + term
         stock_url = stock_url + "&apikey=CYGECFFT35KMZ8E4"
 
         with urllib.request.urlopen(stock_url) as req:
-            stock_days = json.loads(req.read().decode())['Time Series (Daily)']
-            # msg_price = "$" + \
-            #     str(crypto['quotes']['USD']['price']) + "/" + crypto['symbol']
-            # msg_percent = str(crypto['quotes']['USD']
-            #                   ['percent_change_24h']) + "%"
-            # msg_rank = "#" + str(crypto['rank'])
-            # await self.bot.say("```" + msg_price + " " + msg_percent + " " + msg_rank + "```")
-            await self.bot.say(stock_days)
+            response = json.loads(req.read().decode())
+            if 'Error Message' in response:
+                await self.bot.say('Error getting stock: \"' + term.upper() + '\"')
+                return
+            stock_days = response['Time Series (Daily)']
+            current_day_key = sorted(stock_days.keys())[-1]
+            past_day_key = sorted(stock_days.keys())[-2]
+
+            current_day = stock_days[current_day_key]
+            past_day = stock_days[past_day_key]
+
+            current_price = float(current_day['4. close'])
+            past_price = float(past_day['4. close'])
+
+            change = current_price / past_price
+            change_str = ''
+            if change >= 1:
+                change_str = str(round((change - 1) * 100, 2)) + '%'
+            else:
+                change_str = '-' + str(round((1 - change) * 100, 2)) + '%'
+
+            msg = '```' + 'Stock: $' + str(current_price) + '/' + term.upper() + ' ' + change_str + '```'
+            await self.bot.say(msg)
             return
 
 
