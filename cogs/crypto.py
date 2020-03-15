695187626c3effc9249b3a0188e681b9416dbce6
Jeff Cailteux <jethro82@gmail.com>
2020-03-14 17:55:12 -0700

    fix for crypto

--- cogs/crypto.py
+++ cogs/crypto.py
@@ -17,7 +17,7 @@ class Crypto:
     def __init__(self, bot):
         self.bot = bot
         self.data = ""
-        self.listings = ""
+        self.listings = dict()
         self.last_time = 0
 
     @commands.group(pass_context=True)
@@ -35,14 +35,14 @@ class Crypto:
         await get_listings(self)
         crypto_id = 0
 
-        for crypto in self.listings['data']:
+        for crypto in self.listings.values():
             if term.lower() == crypto['name'].lower():
                 crypto_id = crypto['id']
                 break
             if term.lower() == crypto['symbol'].lower():
                 crypto_id = crypto['id']
                 break
-            if term.lower() == crypto['website_slug'].lower():
+            if term.lower() == crypto['slug'].lower():
                 crypto_id = crypto['id']
                 break
 
@@ -50,14 +50,12 @@ class Crypto:
             await self.bot.say("```" + term + " not found" + "```")
             return
 
-        crypto_url = "https://api.coinmarketcap.com/v2/ticker/" + str(crypto_id) + "/"
-        with urllib.request.urlopen(crypto_url) as req:
-            crypto = json.loads(req.read().decode())['data']
-            msg_price = "$" + str(crypto['quotes']['USD']['price']) + "/" + crypto['symbol']
-            msg_percent = str(crypto['quotes']['USD']['percent_change_24h']) + "%"
-            msg_rank = "#" + str(crypto['rank'])
-            await self.bot.say("```" + msg_price + " " + msg_percent + " " + msg_rank + "```")
-            return
+        crypto = self.listings[crypto_id]
+        msg_price = "$" + "{:.2f}".format(crypto['quote']['USD']['price']) + "/" + crypto['symbol']
+        msg_percent = "{:.2f}".format(crypto['quote']['USD']['percent_change_24h']) + "%"
+        msg_rank = "#" + str(crypto['cmc_rank'])
+        await self.bot.say("```" + msg_price + " " + msg_percent + " " + msg_rank + "```")
+        return
 
 
 def setup(bot):
@@ -65,7 +63,13 @@ def setup(bot):
 
 
 async def get_listings(self):
-    listings_url = "https://api.coinmarketcap.com/v2/listings"
-    if self.listings == "":
-        with urllib.request.urlopen(listings_url) as req:
-            self.listings = json.loads(req.read().decode())
+    listings_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=1000&convert=USD"
+    current_time = time.time()
+    if current_time >= self.last_time + (2 * 60):
+        self.last_time = current_time
+        req = urllib.request.Request(listings_url)
+        req.add_header("X-CMC_PRO_API_KEY", self.api_key)
+        req.add_header("Accept", "application/json")
+
+        data = json.loads(urllib.request.urlopen(req).read().decode())
+        self.listings = {x["id"]: x for x in data["data"]}
